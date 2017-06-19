e2fe7c35706c554d9af17c979289b759c2b57114
salamaashoush <salamaashoush@gmail.com>
2017-06-19 18:32:17 +0200

    fixed streaming bug2

--- app/controllers/video.js
+++ app/controllers/video.js
@@ -27,38 +27,36 @@ var upload_video = multer({
 }).single('video');
 router.get('/:videoId/stream', (req, res, next) => {
     Video.findOne({_id:req.params.videoId},(err,video)=>{
-        let path = ''
-        if(video.filename){
-            path = process.cwd() + `/public/uploads/${video.filename}`;
+        let  path = process.cwd() + `/public/uploads/${video.filename}`;
+        if (fs.existsSync(path)) {
+            let stat = fs.statSync(path);
+            let total = stat.size;
+            if (req.headers['range']) {
+                let range = req.headers.range;
+                let parts = range.replace(/bytes=/, "").split("-");
+                let partialstart = parts[0];
+                let partialend = parts[1];
+
+                let start = parseInt(partialstart, 10);
+                let end = partialend ? parseInt(partialend, 10) : total - 1;
+                let chunksize = (end - start) + 1;
+                console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+                let file = fs.createReadStream(path, {start: start, end: end});
+                res.writeHead(206, {
+                    'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': chunksize,
+                    'Content-Type': 'video/mp4'
+                });
+                file.pipe(res);
+            } else {
+                console.log('ALL: ' + total);
+                res.writeHead(200, {'Content-Length': total, 'Content-Type': 'video/mp4'});
+                fs.createReadStream(path).pipe(res);
+            }
         }else{
             return res.status(404).json("file not found")
-        }
-        console.log(path);
-        let stat = fs.statSync(path);
-        let total = stat.size;
-        if (req.headers['range']) {
-            let range = req.headers.range;
-            let parts = range.replace(/bytes=/, "").split("-");
-            let partialstart = parts[0];
-            let partialend = parts[1];
-
-            let start = parseInt(partialstart, 10);
-            let end = partialend ? parseInt(partialend, 10) : total - 1;
-            let chunksize = (end - start) + 1;
-            console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
-
-            let file = fs.createReadStream(path, {start: start, end: end});
-            res.writeHead(206, {
-                'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-                'Accept-Ranges': 'bytes',
-                'Content-Length': chunksize,
-                'Content-Type': 'video/mp4'
-            });
-            file.pipe(res);
-        } else {
-            console.log('ALL: ' + total);
-            res.writeHead(200, {'Content-Length': total, 'Content-Type': 'video/mp4'});
-            fs.createReadStream(path).pipe(res);
         }
     })
 
